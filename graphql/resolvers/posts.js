4965e5a37fbb0baa6bb80ad6e4ee83f6e16ef3ce
Bilal Belagam <bilal.belagam@gmail.com>
2020-10-20 15:57:56 -0400

    finished up the server side stuff for likes and comments functionality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server')
+const { argsToArgsConfig } = require('graphql/type/definition')
 
 const Post = require('../../models/Post')
 const checkAuth = require('../../util/check-auth')
@@ -30,7 +31,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context)
-      console.log(user)
+
+      if (args.body.trim() === '') {
+        throw new Error('post body must not be empty')
+      }
 
       const newPost = new Post({
         body,
@@ -57,6 +61,24 @@ module.exports = {
       } catch (err) {
         throw new Error(err)
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context)
+
+      const post = await Post.findById(postId)
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          post.likes = post.likes.filter(like => like.username !== username)
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          })
+        }
+
+        await post.save()
+        return post
+      } else throw new UserInputError('post not found')
     }
   }
 }
